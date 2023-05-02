fb81a0e0458f972620e75fcec0892d8d284e2a8c
Youssef Ahmed <youssefashwal@outlook.com>
2023-05-02 12:05:39 +0300

    Returns the sum of unicode of all the characters in a string

---
+++ 8th Kyu/uniTotal.js
@@ -0,0 +1,16 @@
+/*
+
+ASCII Total
+
+You'll be given a string, and have to return the sum of all characters as an int. The function should be able to handle all printable ASCII characters.
+
+Examples:
+
+uniTotal("a") == 97
+uniTotal("aaa") == 291
+
+*/
+
+function uniTotal(string) {
+  return [...string].reduce((sum, current) => sum + current.charCodeAt(0), 0);
+}
