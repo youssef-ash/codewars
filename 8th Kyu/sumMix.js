38192b2b77dba60973890900c637de9f2a0d1078
Youssef Ahmed <youssefashwal@outlook.com>
2023-07-09 20:15:08 +0300

    Sums string and numbers in array as numbers

---
+++ 8th Kyu/sumMix.js
@@ -0,0 +1,13 @@
+/*
+
+Sum Mixed Array
+
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+
+*/
+
+function sumMix(x) {
+  return x.reduce((a, b) => parseInt(a) + parseInt(b));
+}
